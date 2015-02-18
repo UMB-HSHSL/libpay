4b5c006da5fc486bd96af2bd2ad7a54951df9ccb
Zak Burke <zburke@hshsl.umaryland.edu>
2015-02-18 16:36:17 -0500

    watch css and js files too

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,10 @@ module.exports = function(grunt) {
     // Livereload is setup for the 35729 port by default
     watch: {
       php: {
-        files: ['**/*.php'],
+        files: ['**/*.php', '**/*.css', '**/*.js'],
         options: {
                 // Start a live reload server on the default port 35729
                 livereload: {
-//                    key: grunt.file.read('/Users/zburke/projects/hshsl/config/server.key'),
-//                    cert: grunt.file.read('/Users/zburke/projects/hshsl/config/server.crt'),
                     key: grunt.file.read('config/livereload.key'),
                     cert: grunt.file.read('config/livereload.crt'),
                     options: { livereload: true },
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Simple controller parent class requires all requests to come in via SSL. 
+ *
+ */
 class MY_Controller extends CI_Controller
 {
-  public function is_post()
-  {
-    return 'post' == strtolower($_SERVER['REQUEST_METHOD']);
-  }
+    public function __construct()
+    {
+        parent::__construct();
+        
+        // force SSL
+        if (! isset($_SERVER['HTTPS']) || 'on' != $_SERVER['HTTPS']) {
+            redirect("https://{$_SERVER['SERVER_NAME']}{$_SERVER['REQUEST_URI']}");
+            exit();
+        }
+    }
+
+    /**
+     * Return boolean TRUE if this is a post request; boolean FALSE otherwise. 
+     * @return boolean
+     */
+    public function is_post()
+    {
+        return 'post' == strtolower($_SERVER['REQUEST_METHOD']);
+    }
 }
