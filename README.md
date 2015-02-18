c677e671fbd2ba57e81500e10f2e60a1b31067f4
Zak Burke <zburke@hshsl.umaryland.edu>
2015-02-18 11:30:27 -0500

    repository cleanup and deployment refinements

--- README.md
+++ README.md
@@ -1,24 +1,40 @@
-Simple-Bootstrap-Stripe-Payment-Form
+LibPay
 ====================================
+Accept on-line payments from patrons. This application was forked from
+https://github.com/myg0v/Simple-Bootstrap-Stripe-Payment-Form. It uses
+Stripe to handle payment processing, including submission of the user's
+credit card data directly to Stripe, before the payment form is submitted
+to our own servers, so we do not need to worry about PCI compliance.
 
-Simple Bootstrap Stripe example payment form.
-Replace "Stripe Publishable Key" & "Stripe Secret Key" in index.php
-if you want to test it.
+The backend is written in PHP 5.2.x using CodeIgniter 2.2.1.
 
-Improvements are always welcome. GitHub pull requests are even better!
-
-Assembled by Daniel Schröter
-
-Features
+Manifest
 ===============
-* Form validation
-* Generate Token via Stripe JS
-* Responsive design
-* Warning if JavaScript is disabled
-* Charge single amounts with Stripe
+application - application PHP code
+config - default config files
+htdocs - static assets and the CodeIgniter index.php file
+htdocs/web.config - denies access to application and config directories
+system - CodeIgniter framework
+deploy.sh - deployment script
+web.config - IIS config file denies web-access to application directories
+
+Deployment
+===============
+First release:
+1. Run deploy.sh; this will install the application.
+2. Rename the config files in the deployment directory's "config" directory
+   from "somefile.php-default" to "somefile.php" and edit them as needed.
+   These files will NOT be overwritten by subsequent releases.
+
+Subsequent releases:
+1. Run deploy.sh.
+
+
 
 Used Components
 ===============
+
+=> Stripe - http://stripe.com
 
 => jQuery (v1.11.0) - http://jquery.com/
 
--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,9 @@
 #
 # generic git deployment script
 #
-# assumes a copy of the repository to deploy is checked out at $SRC/$REPO.
-# specify destination directory with $DST.
+# usage: $0 <tag-name> [deployment-dir]
 #
-# usage: $0 <repo-name> <tag-name>
+# deployment-dir is hardcoded but may be overridden in at runtime.
 #
 # Assumes releases are tagged on the master branch. This script performs an
 # archive action to create a directory named for a given repository and tag
@@ -17,18 +16,20 @@
 # 	git tag -a 0-0-0 -m "tag comment"
 # 	git push origin --tags
 #
-# and are pulled by "git fetch --tags".
-#
 # Zak Burke, zburke@hshsl.umaryland.edu
 # 2015-02-16
 #
 
 
-# repository should be checked out to this directory
-SRC=/home/zburke/projects/releases
-
-# document root; symlink in this directory will point to repository
-# exported into $SRC, above
+# default deployment directory; may be overridden by passing in a
+# path on the commandline after the release tag.
+#
+# application will be deployed into this directory with a symlink
+# pointing from the repository name to a versioned directory, i.e.
+#
+#     cd <deployment-directory>
+#     ln -s <repository-name>-<tag> <repository-name>
+#
 DST=/cygdrive/w/HSHSL/bin/test/zburke
 
 
@@ -36,46 +37,37 @@ DST=/cygdrive/w/HSHSL/bin/test/zburke
 # -- NOTHING TO CONFIGURE BELOW
 # --
 
-if [[ ! -d $SRC ]]; then
-    echo "The source directory '$SRC' does not exist."
+DIST=$PWD/dist
+
+# extract the name of the repository
+REPO=`git config remote.origin.url| sed -e 's/.*\/\(.*\)\.git/\1/'`
+
+
+
+if [[ -z $1 ]]; then
+    echo "usage: $0 <release-tag> [destination]"
+    echo "recent tags:"
+    git tag -n1 | tail -5
     exit 1;
 fi
 
+TAG=$1
 
+if [[ $2 ]]; then
+  DST=$2
+fi
 if [[ ! -d $DST ]]; then
     echo "The deployment directory '$DST' does not exist."
     exit 1;
 fi
 
-if [[ -z $1 ]]; then
-    echo "usage: $0 <repo-name> <release-tag>"
-    exit 1;
-fi
-REPO=$1
-
-if [[ -z $2 ]]; then
-    echo "usage: $0 <repo-name> <release-tag>"
-    echo "recent tags:"
-    cd $SRC/$REPO
-    git tag -n1 | tail -5
-    exit 1;
-fi
-
-TAG=$2
-
-if [[ ! -d $SRC/$REPO ]]; then
-    echo "The source directory '$SRC' does not contain the repository '$REPO'."
-    exit 1;
-fi
-
 
 echo "syncing the repository..."
-cd $SRC/$REPO
 git fetch --tags -q
 
 if ! git tag | grep -q $TAG; then
     echo "The tag '$TAG' could not be found in the repository."
-    echo "usage: $0 <repo-name> <release-tag>\nrecent tags:"
+    echo "usage: $0 <release-tag> [destination]\nrecent tags:"
     git tag -n1 | tail -5
     exit 1;
 fi
@@ -83,18 +75,27 @@ fi
 
 if [[ ! -d $SRC/$REPO-$TAG ]]; then
     echo "exporting release $TAG..."
-    mkdir $SRC/$REPO-$TAG
-    git archive $TAG | tar -x -C $SRC/$REPO-$TAG
+    mkdir -p $DIST/$REPO-$TAG
+    git archive $TAG | tar -x -C $DIST/$REPO-$TAG
 else
     echo "release $TAG already in place; deploying existing copy"
 fi
-
-cd $SRC
-echo "installing release $TAG..."
+cd $DIST
 tar czf $REPO-$TAG.tgz $REPO-$TAG
-cp $SRC/$REPO-$TAG.tgz $DST/
+
+echo "installing release $TAG at $DST..."
+cp $DIST/$REPO-$TAG.tgz $DST/
 cd $DST
 tar xzf $REPO-$TAG.tgz
+
+# deny web access to application and config directories
+cp $REPO-$TAG/htdocs/web.config ./
+
+# stupid stupid STUPID IIS7 will not traverse a shortcut and
+# Windows requires Administrative access to use mklink, so instead
+# The Scriptmeister deploys by makin' copies.
+#
+# prepare copies (slow) then move them into place (fast).
 cp -R $REPO-$TAG $REPO.NEW
 if [[ -d $REPO ]]; then
 	mv $REPO $REPO.OLD
@@ -102,8 +103,26 @@ fi
 mv $REPO.NEW $REPO
 
 if [[ -d $REPO.OLD ]]; then
-	rm -rf $REPO;
+	rm -rf $REPO.OLD;
 fi
-	
-echo "DONE!"
 
+cp -R $REPO/htdocs/assets assets.NEW
+if [[ -d assets ]]; then
+  mv assets assets.OLD
+fi
+mv assets.NEW assets
+
+if [[ -d assets.OLD ]]; then
+  rm -rf assets.OLD;
+fi
+
+if [[ ! -d config ]]; then
+  cp -R $REPO/config ./
+fi
+
+if [[ ! -f index.php ]]; then
+  cp -R $REPO/htdocs/index.php ./
+fi
+
+
+echo "DONE!"
