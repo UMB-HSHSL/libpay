e3f57db22a673617b2411235169ded5f74dcfd98
Zak Burke <zburke@hshsl.umaryland.edu>
2015-03-20 09:57:48 -0400

    label required fields

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -1,3 +1,5 @@
+<p><span class="text-error">*</span> - Indicates required fields.</p>
+
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('', 'id="payment-form" class="form-horizontal" autocomplete="off"')?>
@@ -21,7 +23,7 @@
 
     <!-- Fee Category-->
     <div class="form-group">
-      <label class="col-sm-4 control-label" for="hshsl_category">Fee Category</label>
+      <label class="col-sm-4 control-label" for="hshsl_category"><span class="text-error">*</span> Fee Category</label>
       <div class="col-sm-6">
           <select name="hshsl_category" id="hshsl_category" required>
             <option value="Library Fines" selected="selected">Library Fines</option>
@@ -51,7 +53,7 @@
 
   <!-- Amount -->
   <div class="form-group">
-    <label class="col-sm-4 control-label" for="textinput">Payment Amount</label>
+    <label class="col-sm-4 control-label" for="textinput"><span class="text-error">*</span> Payment Amount</label>
     <div class="col-sm-6">
         <div class="form-inline">
             $ <input type="text" size="3" name="hshsl_amount" placeholder="0.00" class="amount form-control hshsl-amount">
@@ -61,9 +63,9 @@
 
   <!-- Patron Name -->
   <div class="form-group">
-    <label class="col-sm-4 control-label" for="patron_name">Patron Name</label>
+    <label class="col-sm-4 control-label" for="patron_name"><span class="text-error">*</span> Patron Name</label>
     <div class="col-sm-6">
-      <input type="text" name="patron_name" placeholder="Patron Name" class="patron_name form-control">
+      <input type="text" name="patron_name" placeholder="Patron Name" class="patron_name form-control" required>
     </div>
   </div>
 
@@ -79,17 +81,17 @@
 
   <!-- Phone -->
   <div class="form-group">
-    <label class="col-sm-4 control-label" for="phone">Phone</label>
+    <label class="col-sm-4 control-label" for="phone"><span class="text-error">*</span> Phone</label>
     <div class="col-sm-6">
-      <input type="text" name="phone" maxlength="12" placeholder="phone (111-222-3333)" class="phone form-control">
+      <input type="text" name="phone" maxlength="12" placeholder="phone (111-222-3333)" class="phone form-control" required>
     </div>
   </div>
 
   <!-- Email -->
   <div class="form-group">
-    <label class="col-sm-4 control-label" for="email">E-mail</label>
+    <label class="col-sm-4 control-label" for="email"><span class="text-error">*</span> E-mail</label>
     <div class="col-sm-6">
-      <input type="text" name="email" maxlength="65" placeholder="Email" class="email form-control">
+      <input type="text" name="email" maxlength="65" placeholder="Email" class="email form-control" required>
     </div>
   </div>
 
@@ -108,33 +110,33 @@
 
   <!-- Street -->
   <div class="form-group">
-    <label class="col-sm-4 control-label" for="street">Street</label>
+    <label class="col-sm-4 control-label" for="street"><span class="text-error">*</span> Street</label>
     <div class="col-sm-6">
-      <input type="text" name="street" placeholder="Street" class="address form-control">
+      <input type="text" name="street" placeholder="Street" class="address form-control" required>
     </div>
   </div>
 
   <!-- City -->
   <div class="form-group">
-    <label class="col-sm-4 control-label" for="city">City</label>
+    <label class="col-sm-4 control-label" for="city"><span class="text-error">*</span> City</label>
     <div class="col-sm-6">
-      <input type="text" name="city" placeholder="City" class="city form-control">
+      <input type="text" name="city" placeholder="City" class="city form-control" required>
     </div>
   </div>
 
   <!-- State -->
   <div class="form-group">
-    <label class="col-sm-4 control-label" for="state">State</label>
+    <label class="col-sm-4 control-label" for="state"><span class="text-error">*</span> State</label>
     <div class="col-sm-6">
-      <input type="text" name="state" maxlength="65" placeholder="State (e.g. MD)" class="state form-control">
+      <input type="text" name="state" maxlength="65" placeholder="State (e.g. MD)" class="state form-control" required>
     </div>
   </div>
 
   <!-- Postcal Code -->
   <div class="form-group">
-    <label class="col-sm-4 control-label" for="zip">Postal Code</label>
+    <label class="col-sm-4 control-label" for="zip"><span class="text-error">*</span> Postal Code</label>
     <div class="col-sm-6">
-      <input type="text" name="zip" maxlength="9" placeholder="Postal Code" class="zip form-control">
+      <input type="text" name="zip" maxlength="9" placeholder="Postal Code" class="zip form-control" required>
     </div>
   </div>
 
@@ -145,27 +147,27 @@
 
     <!-- Card Holder Name -->
     <div class="form-group">
-      <label class="col-sm-4 control-label"  for="cardholdername">Card Holder's Name</label>
+      <label class="col-sm-4 control-label"  for="cardholdername"><span class="text-error">*</span> Card Holder's Name</label>
       <div class="col-sm-6">
-        <input type="text" name="cardholdername" maxlength="70" placeholder="Card Holder Name" class="card-holder-name form-control">
+        <input type="text" name="cardholdername" maxlength="70" placeholder="Card Holder Name" class="card-holder-name form-control" required>
       </div>
     </div>
 
     <!-- Card Number -->
     <div class="form-group">
-      <label class="col-sm-4 control-label" for="cardnumber">Card Number</label>
+      <label class="col-sm-4 control-label" for="cardnumber"><span class="text-error">*</span> Card Number</label>
       <div class="col-sm-6">
-        <input type="text" id="cardnumber" maxlength="19" placeholder="Card Number" class="card-number form-control">
+        <input type="text" id="cardnumber" maxlength="19" placeholder="Card Number" class="card-number form-control" required>
         <p class="help-block">The following cards are accepted: <?php echo implode(", ", array_map('ucfirst', config_item('stripe_valid_brands')))?>.</p>
         </div>
     </div>
 
     <!-- Expiry-->
     <div class="form-group">
-      <label class="col-sm-4 control-label" for="textinput">Card Expiry Date</label>
+      <label class="col-sm-4 control-label" for="textinput"><span class="text-error">*</span> Card Expiry Date</label>
       <div class="col-sm-6">
         <div class="form-inline">
-          <select name="select2" data-stripe="exp-month" class="card-expiry-month stripe-sensitive required form-control">
+          <select name="select2" data-stripe="exp-month" class="card-expiry-month stripe-sensitive required form-control" required>
             <option value="01" selected="selected">01</option>
             <option value="02">02</option>
             <option value="03">03</option>
@@ -180,7 +182,7 @@
             <option value="12">12</option>
           </select>
           <span> / </span>
-          <select name="select2" data-stripe="exp-year" class="card-expiry-year stripe-sensitive required form-control">
+          <select name="select2" data-stripe="exp-year" class="card-expiry-year stripe-sensitive required form-control" required>
           </select>
           <script type="text/javascript">
             var select = $(".card-expiry-year"),
@@ -196,9 +198,9 @@
 
     <!-- CVV -->
     <div class="form-group">
-      <label class="col-sm-4 control-label" for="cvv">CVV/CVV2</label>
+      <label class="col-sm-4 control-label" for="cvv"><span class="text-error">*</span> CVV/CVV2</label>
       <div class="col-sm-3">
-        <input type="text" id="cvv" placeholder="CVV" maxlength="4" class="card-cvc form-control">
+        <input type="text" id="cvv" placeholder="CVV" maxlength="4" class="card-cvc form-control" required>
       </div>
     </div>
 
